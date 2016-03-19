218eca9ad8b75a7bd9c41bef59ec21da46747068
Brent Robbins <brent@variantstudios.com>
2016-03-18 22:18:16 -0600

    Added a simple loading image.

--- _site/assets/js/controller.js
+++ _site/assets/js/controller.js
@@ -1,14 +1,42 @@
 var app = angular.module('mainApp', ['angularUtils.directives.dirPagination'])
 
 .controller('DirectoryCtrl', function($scope, $http) {
+
+	$scope.loading = true;
+	
 	$http.get('/directory.json')
 	.success(function(response) {
 		$scope.businesses = response.records;
-		console.log("test 1");
-	});
+	})
+    .catch(function (err) {
+      // Log error somehow.
+    })
+    .finally(function () {
+      // Hide loading spinner whether our call succeeded or failed.
+      $scope.loading = false;
+    });
 	$http.get('/classifications.json')
 	.success(function(data) {
 		$scope.classifications = data.records;
-		console.log("test 2");
 	})
 });
+
+/*
+
+app.controller('myCtrl', function ($scope, $http) {  
+  // Show loading spinner.
+  $scope.loading = true;
+  $http.get('/some/awesome/content')
+    .success(function (data) {
+      // Do stuff with data.
+    })
+    .catch(function (err) {
+      // Log error somehow.
+    })
+    .finally(function () {
+      // Hide loading spinner whether our call succeeded or failed.
+      $scope.loading = false;
+    });
+});
+
+*/
